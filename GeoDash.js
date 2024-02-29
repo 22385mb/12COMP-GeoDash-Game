c04dc596964a4f0e8ddadde3dfc54e41f5ce3db7
22385mb <161382701+22385mb@users.noreply.github.com>
2024-02-29 15:10:07 +1300

    Update GeoDash.js
    Obstacle Creation Working

--- GeoDash.js
+++ GeoDash.js
@@ -12,10 +12,14 @@ const SCREENHEIGHT = 540;
 const PLAYER_ON_GROUND = 493;
 
 const PLAYER_SIZE = 50;
+const OBSTACLE_HEIGHT = PLAYER_SIZE;
+const OBSTACLE_WIDTH = PLAYER_SIZE;
 
 var score = 0;
+var spawnDist = 0;
 
-
+//Creating global variable for obstacles
+var obstacles;
 
 /*******************************************************/
 // setup()
@@ -23,28 +27,23 @@ var score = 0;
 function setup() {
     console.log("setup: ");
     cnv = new Canvas(SCREENWIDTH, SCREENHEIGHT);
-    world.gravity.y = 80;
+    world.gravity.y = 120;
     
     // Creating Sprites
     createSprites();
+    obstacles = new Group();
     
     // Key Presses
     document.addEventListener("keydown", 
         function(event) {
-            console.log(player.y)
             if(player.y > PLAYER_ON_GROUND) {
-                console.log("JUMPED" + player.y);
-                player.vel.y = -27;
+                player.vel.y = -33;
             }
         }
     );
-    //document.addEventListener("keyup", 
-       // function(event) {
-         //   player.vel.y = 0;
-                
-        //}
-    //);
     
+    // Player and Obstacles Collision
+    player.collides(obstacles, endGame);
 }
 
 /*******************************************************/
@@ -52,15 +51,19 @@ function setup() {
 /*******************************************************/
 function draw() {
     background('lightGrey')
- 
+    player.rotation = 0;
+    // Creating Multiple Obstacles
+    
+    
 }
 
 /******************************************************/
 // Functions
 /*****************************************************/
+// Creating the Sprites
 function createSprites() {
     // Player
-    player = new Sprite(100, 100, PLAYER_SIZE, PLAYER_SIZE, 'd');
+    player = new Sprite(100, SCREENHEIGHT - 255, PLAYER_SIZE, PLAYER_SIZE, 'd');
     player.color = 'lightblue';
     
     // Ground
@@ -68,10 +71,19 @@ function createSprites() {
     ground.color = "green";
 }
 
-//function obstacles() {
-    //obstacle = new Sprite(200, SCREENHEIGHT - 20, 50, 50, 'd');
-    //obstacle.color = "red";
-//}
+function newObstacle() {
+    // Obstacles
+    obstacle = new Sprite(SCREENWIDTH + 100, SCREENHEIGHT - 45, OBSTACLE_WIDTH, OBSTACLE_HEIGHT, 'k');
+    obstacle.color = 'red';
+    obstacle.vel.x = -10;
+    
+    //Obstacles Group
+    obstacles.add(obstacle);
+}
+
+function endGame(_player, _obstacle) {
+    console.log("YOU LOSE");
+}
 
 /*******************************************************/
 //  END OF APP
